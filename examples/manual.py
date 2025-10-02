8e0d573498b4c6fa8dff9fefbf00336c5ce139eb
David Konsumer <konsumer@jetboystudio.com>
2025-10-02 03:06:39 -0700

    basic decrypt working

--- examples/manual.py
+++ examples/manual.py
@@ -1,6 +1,6 @@
 import demo_data
 import RNS
-import threading
+import textwrap
 
 # this loads a Packet, with no side-effects 
 def load_packet(raw):
@@ -58,37 +58,27 @@ def load_destination(identity, direction, type, app_name, *aspects):
 
 # parse an announce packet and add to RNS.Identity.known_ratchets
 def process_announce(packet):
-    keysize       = RNS.Identity.KEYSIZE//8
-    ratchetsize   = RNS.Identity.RATCHETSIZE//8
-    name_hash_len = RNS.Identity.NAME_HASH_LENGTH//8
-    destination_hash = packet.destination_hash
-    public_key = packet.data[:keysize]
-    if packet.context_flag == RNS.Packet.FLAG_SET:
-        ratchet     = packet.data[keysize+name_hash_len+10:keysize+name_hash_len+10+ratchetsize]
-    else:
-        ratchet     = b""
-    RNS.Identity.known_ratchets[destination_hash] = ratchet
+    # TODO: generate a ratchet for this destination
+    pass
 
 
-
-# parse & decrypt DATA packet with recipient identity (private & public key) & stored announce identities (pubkey, ratchet info, etc)
-def process_data(packet, recipient):
-    decryptedBytes = recipient.identity.decrypt(packet.data, ratchets=demo_data.ratchets)
+# parse & decrypt DATA packet with recipient identity (private & public key) & ratchets
+def process_data(packet, identity):
+    decryptedBytes = identity.decrypt(packet.data, ratchets=demo_data.ratchets)
     return RNS.vendor.umsgpack.unpackb(decryptedBytes[80:])
-    
 
 
 clientA = RNS.Identity.from_bytes(demo_data.keys['clientA'])
 clientB = RNS.Identity.from_bytes(demo_data.keys['clientB'])
 clientA_addr = RNS.Destination.hash(clientA, "lxmf", "delivery")
 clientB_addr = RNS.Destination.hash(clientB, "lxmf", "delivery")
-recipient_destinations = {
-    clientA_addr: load_destination(clientA, RNS.Destination.IN, RNS.Destination.SINGLE, "lxmf", "delivery"),
-    clientB_addr: load_destination(clientB, RNS.Destination.IN, RNS.Destination.SINGLE, "lxmf", "delivery")
+identities = {
+    clientA_addr: clientA,
+    clientB_addr: clientB
 }
 
-print(f"Client A LXMF Address: {recipient_destinations[clientA_addr].hexhash}")
-print(f"Client B LXMF Address: {recipient_destinations[clientB_addr].hexhash}")
+print(f"Client A LXMF Address: {clientA_addr.hex()}")
+print(f"Client B LXMF Address: {clientB_addr.hex()}")
 
 for p in demo_data.packets:
     packet = load_packet(p)
@@ -97,16 +87,15 @@ for p in demo_data.packets:
         print(f"ANNOUNCE ({packet.destination_hash.hex()})")
         if RNS.Identity.validate_announce(packet, True):
             process_announce(packet)
-            print("  Added announce")
+            print("  Valid")
         else:
-            print("  Skipped (invalid)")
+            print("  Invalid")
     
     if packet.packet_type == RNS.Packet.DATA:
         print(f"DATA ({packet.destination_hash.hex()})")
         try:
-            recipient = recipient_destinations[packet.destination_hash]
-            ts, title, content, fields = process_data(packet, recipient)
-            print(f"  Received message:\n  {content.decode('utf8')}")
+            ts, title, content, fields = process_data(packet, identities[packet.destination_hash])
+            print(f"  Received message:\n{textwrap.indent(content.decode('utf8'), '    ')}")
 
         except Exception as e:
             print(f"  Error: {e}")
