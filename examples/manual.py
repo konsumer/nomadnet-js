6584700d36b1048f06851b4769cb8dccefd887f4
David Konsumer <konsumer@jetboystudio.com>
2025-10-02 03:07:52 -0700

    basic decrypt working

--- examples/manual.py
+++ examples/manual.py
@@ -12,55 +12,10 @@ def load_packet(raw):
   packet.receiving_interface = None
   return packet
 
-# load a destination, with no side-effects 
-def load_destination(identity, direction, type, app_name, *aspects):
-    dest = RNS.Destination.__new__(RNS.Destination)
-    dest.accept_link_requests = True
-    dest.request_handlers = {}
-    dest.type = type
-    dest.direction = direction
-    dest.proof_strategy = RNS.Destination.PROVE_NONE
-    dest.ratchets = None
-    dest.ratchets_path = None
-    dest.ratchet_interval = RNS.Destination.RATCHET_INTERVAL
-    dest.ratchet_file_lock = threading.Lock()
-    dest.retained_ratchets = RNS.Destination.RATCHET_COUNT
-    dest.latest_ratchet_time = None
-    dest.latest_ratchet_id = None
-    dest.__enforce_ratchets = False
-    dest.mtu = 0
-
-    dest.path_responses = {}
-    dest.links = []
-
-    if identity == None and direction == RNS.Destination.IN and dest.type != RNS.Destination.PLAIN:
-        identity = RNS.Identity()
-        aspects = aspects+(identity.hexhash,)
-
-    if identity == None and direction == RNS.Destination.OUT and dest.type != RNS.Destination.PLAIN:
-        raise ValueError("Can't create outbound SINGLE destination without an identity")
-
-    if identity != None and dest.type == RNS.Destination.PLAIN:
-        raise TypeError("Selected destination type PLAIN cannot hold an identity")
-
-    dest.identity = identity
-    dest.name = RNS.Destination.expand_name(identity, app_name, *aspects)
-
-    # Generate the destination address hash
-    dest.hash = RNS.Destination.hash(dest.identity, app_name, *aspects)
-    dest.name_hash = RNS.Identity.full_hash(dest.expand_name(None, app_name, *aspects).encode("utf-8"))[:(RNS.Identity.NAME_HASH_LENGTH//8)]
-    dest.hexhash = dest.hash.hex()
-
-    dest.default_app_data = None
-    dest.callback = None
-    dest.proofcallback = None
-    return dest
-
 # parse an announce packet and add to RNS.Identity.known_ratchets
 def process_announce(packet):
     # TODO: generate a ratchet for this destination
     pass
-
 
 # parse & decrypt DATA packet with recipient identity (private & public key) & ratchets
 def process_data(packet, identity):
@@ -102,5 +57,4 @@ for p in demo_data.packets:
     
     if packet.packet_type == RNS.Packet.PROOF:
         print(f"PROOF ({packet.destination_hash.hex()})")
-
 
