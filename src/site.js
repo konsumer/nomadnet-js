11bd509f011477451a2eb55eff537a5e24cdafb3
David Konsumer <konsumer@jetboystudio.com>
2025-10-22 21:04:30 -0700

    setting things up for message-sending

--- src/site.js
+++ src/site.js
@@ -13,9 +13,14 @@ const inputPrivateKey = document.getElementById('inputPrivateKey')
 const buttonGenerate = document.getElementById('buttonGenerate')
 const buttonSet = document.getElementById('buttonSet')
 const buttonDeletePeers = document.getElementById('buttonDeletePeers')
+const buttonCopy = document.getElementById('buttonCopy')
 const lxmfAddress = document.getElementById('lxmfAddress')
 const buttonAnnounce = document.getElementById('buttonAnnounce')
 const peerList = document.getElementById('peerList')
+const dialogPopupMessage = document.getElementById('dialogPopupMessage')
+const inputSendAddress = document.getElementById('inputSendAddress')
+const buttonSendMessage = document.getElementById('buttonSendMessage')
+const inputSendBody = document.getElementById('inputSendBody')
 
 const packetTypeNames = {}
 packetTypeNames[rns.PACKET_DATA] = 'DATA'
@@ -24,6 +29,12 @@ packetTypeNames[rns.PACKET_LINKREQUEST] = 'LINKREQUEST'
 packetTypeNames[rns.PACKET_PROOF] = 'PROOF'
 
 const decoder = new TextDecoder()
+
+let identity
+const messages = {}
+const ratchets = []
+let ratchetPub
+let peers = JSON.parse(localStorage.peers || '{}')
 
 // ANNOUNCE yourself
 function announce() {
@@ -67,14 +78,13 @@ function handlePacket(data) {
       message.sourceHex = bytesToHex(message.sourceHash)
       const title = decoder.decode(message.title)
       const content = decoder.decode(message.content)
-
       customElements.get('pop-notify').notifyHtml(title.trim() === '' ? `<strong>${message.sourceHex}</strong><br/>${content}` : `<strong>${message.sourceHex}</strong><br/><strong>${title}</strong><br/>${content}`)
-
       console.log({ ...message, title, content, timestamp: new Date(message.timestamp * 1000) })
     }
   }
 
   if (packet.packetType === rns.PACKET_PROOF) {
+    // TODO: verify PROOF
   }
 }
 
@@ -90,15 +100,8 @@ function connect() {
     }
   })
 }
-connect()
 
-let identity
-const messages = {}
-const ratchets = []
-let ratchetPub
-let peers = JSON.parse(localStorage.peers || '{}')
-updatePeers()
-
+// update the UI list of peers
 function updatePeers() {
   localStorage.peers = JSON.stringify(peers)
   peerList.innerHTML = ''
@@ -106,12 +109,25 @@ function updatePeers() {
     const li = document.createElement('li')
     li.className = 'mono'
     li.textContent = announce.destinationHex
+
+    // TODO: disabled until I fix message-sending
+    if (identity && false) {
+      li.title = `Click to message ${announce.destinationHex}`
+      li.className += ' pointer'
+      li.onclick = () => {
+        inputSendAddress.value = announce.destinationHex
+        inputSendBody.value = ''
+        dialogPopupMessage.showModal()
+      }
+    }
+
     peerList.appendChild(li)
   })
 }
 
 buttonGenerate.addEventListener('click', (e) => {
   inputPrivateKey.value = bytesToHex(randomBytes(64))
+  buttonCopy.classList.remove('hidden')
 })
 
 buttonAnnounce.addEventListener('click', announce)
@@ -126,8 +142,10 @@ buttonSet.addEventListener('click', (e) => {
     privkeyHolder.remove()
     lxmfAddress.value = identity.destinationHex
     lxmfAddress.parentElement.removeAttribute('hidden')
-    // setInterval(announce, 60000 * 60) // announce every hour
+    setInterval(announce, 60000) // announce every minute
     announce()
+    // update the click-handlers of peers becayuse now you have an identity
+    updatePeers()
   }
 })
 
@@ -136,3 +154,30 @@ buttonDeletePeers.addEventListener('click', (e) => {
   peers = {}
   updatePeers()
 })
+
+buttonCopy.addEventListener('click', (e) => {
+  navigator.clipboard.writeText(inputPrivateKey.value).then(() => {
+    customElements.get('pop-notify').notifyHtml('Private key copied!')
+  })
+})
+
+buttonSendMessage.addEventListener('click', (e) => {
+  const message = {
+    from: identity,
+    to: inputSendAddress.value,
+    body: inputSendBody.value
+  }
+
+  // TODO: actually build & send message
+
+  console.log('send message', message)
+})
+
+for (const b of document.querySelectorAll('.closeDialogPopupMessage')) {
+  b.addEventListener('click', () => {
+    dialogPopupMessage.close()
+  })
+}
+
+connect()
+updatePeers()
